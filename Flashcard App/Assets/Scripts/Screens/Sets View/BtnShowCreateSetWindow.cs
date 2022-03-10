d0b767a67e888e46e972aeb2e0458f77dc3b3ad0
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2022-03-10 22:31:42 +0000

    Set creation redone

--- Flashcard App/Assets/Scripts/Screens/Sets View/BtnShowCreateSetWindow.cs
+++ Flashcard App/Assets/Scripts/Screens/Sets View/BtnShowCreateSetWindow.cs
@@ -8,13 +8,15 @@ public class BtnShowCreateSetWindow : MonoBehaviour
 {
     private Button btnShowCreateSetWindow;
 
-    private void Awake()
-    {
-        btnShowCreateSetWindow.onClick.AddListener(OnShowCreateSetWindowButtonPressed); 
-    }
+    [Header("Prefab To Spawn")]
+    [SerializeField] private ModalWindow_CreateSet modalWindow_CreateSet;
+
+    private void Awake() => btnShowCreateSetWindow = GetComponent<Button>();
+    private void Start() => btnShowCreateSetWindow.onClick.AddListener(OnShowCreateSetWindowButtonPressed);
 
     private void OnShowCreateSetWindowButtonPressed()
     {
         //Enable modal window.
+        modalWindow_CreateSet.AnimateWindow();
     }
 }
---
+++ Flashcard App/Assets/Scripts/Screens/Sets View/ModalWindow_CreateSet.cs
@@ -0,0 +1,50 @@
+using Michsky.UI.ModernUIPack;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModalWindow_CreateSet : CustomModalWindow
+{
+    [Header("Window Components")]
+    [SerializeField] private CustomInputField setNameInputField;
+    [SerializeField] private SwitchManager defaultSetSwitch;
+    [SerializeField] private Button btnCancelCreation;
+    [SerializeField] private Button btnCreateSet;
+
+    private bool CanCreateSet { get { return setNameInputField.inputField.text != string.Empty; } }
+
+    public override void Awake() => base.Awake();
+
+
+    public override void Start()
+    {
+        btnCancelCreation.onClick.AddListener(CloseAndResetInputField);
+        btnCreateSet.onClick.AddListener(OnCreateSetButtonPressed);
+    }
+
+    private void CloseAndResetInputField() 
+    {
+        setNameInputField.ClearText(updateInstant: true);
+        CloseWindow();
+    }
+    private void OnCreateSetButtonPressed() 
+    {
+        if(CanCreateSet)
+        {
+            //Create set.
+            string setName = setNameInputField.inputField.text;
+            bool setDefaultSet = defaultSetSwitch.isOn;
+
+            //Create new set. 
+            //Set as default where???
+            Set newSet = new Set(setName);
+
+            CloseAndResetInputField();
+        }
+        else
+        {
+            //Display error? or something
+        } 
+    }
+}
