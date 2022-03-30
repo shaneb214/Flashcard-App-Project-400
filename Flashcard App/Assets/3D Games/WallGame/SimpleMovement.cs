5bb915c26a6fa994e9e349e2ad4e0982cd349ae1
ShaneB22 <47157867+ShaneB22@users.noreply.github.com>
2022-03-30 17:10:46 +0100

    Added basic files for wall game

--- Flashcard App/Assets/3D Games/WallGame/SimpleMovement.cs
+++ Flashcard App/Assets/3D Games/WallGame/SimpleMovement.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 public class SimpleMovement : MonoBehaviour
 {
     private CharacterController characterController;
+    private Animator animator;
     [SerializeField] private float movementSpeed;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        //Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 1f);
+        //Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 1f);
 
         characterController.SimpleMove(input * movementSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("fsafsaf");
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.normal.y <= 0.7f)
+        {
+            animator.SetBool("Fall", true);
+            print(hit.gameObject.name);
+        }
+            
     }
 }
